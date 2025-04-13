b8418b96db2d06f32685163878f61ab9963369df
LevLaptop <luteranlevi@gmail.com>
2025-04-13 12:43:20 +0200

    v0.2.5 AuthContainer.tsx change

--- src/components/AuthContainer.tsx
+++ src/components/AuthContainer.tsx
@@ -5,19 +5,22 @@ interface AuthContainerInterface {
     children: JSX.Element;
 }
 
-const AuthContainer = ({children}: AuthContainerInterface) => {
-    return <div className="auth-container">
-        <Center><Image src="/logo.png" alt="img" w={150} mt={30}/></Center>
-        <Center>
-            <Paper radius="md" p="xl" withBorder maw={600} m={10}>
-                <Text size="lg" fw={500}>
-                    Raktár
-                </Text>
-                <Divider my="lg"/>
-                {children}
-            </Paper>
-        </Center>
-    </div>
-}
+const AuthContainer = ({ children }: AuthContainerInterface) => {
+    return (
+        <div className="auth-container">
+            <Center><Image src="/logo.png" alt="img" w={150} mt={30} /></Center>
+            <Center>
+                <Paper radius="md" p="xl" withBorder maw={600} m={10}>
+                    <Text size="lg" fw={500} ta="center">
+                        Üdvözöljük a Raktár app-ban!
+                    </Text>
+                    <Divider my="lg" />
+                    {children}
+                </Paper>
+            </Center>
+        </div>
+    );
+};
+
 
 export default AuthContainer;
